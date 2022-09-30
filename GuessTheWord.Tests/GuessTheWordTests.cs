e6d748f0ec396592f7e14069b8ab253c8fda7502
Timo Hermans <tmhermans@gmail.com>
2022-09-30 07:43:05 +0200

    Add two example unit tests

---
+++ GuessTheWord.Tests/GuessTheWordTests.cs
@@ -0,0 +1,23 @@
+﻿using GuessTheWord.Core;
+
+namespace GuessTheWord.Tests;
+
+public class GuessTheWordTests
+{
+    [Fact]
+    public void Starts_a_new_game_with_the_word_completely_masked()
+    {
+        var game = new Game("timo");
+
+        var toGuess = game.Word;
+
+        Assert.Equal("****", toGuess);
+    }
+
+    [Fact]
+    public void A_default_game_has_5_tries_by_default()
+    {
+        var game = new Game("timo");
+        Assert.Equal(5, game.AmountOfTriesLeft);
+    }
+}
